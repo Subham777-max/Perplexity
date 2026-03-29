f8913d6fc9bdfb4cbcb92b5f17c55f6e52321652
Subham777-max <subhamray865@gmail.com>
2026-03-29 19:30:16 +0530

    backend is setup and initial plan is created

---
+++ Backend/src/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+async function connectDb(){
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Mongodb connected successsfully");
+    }catch(err){
+        console.log("Mongodb connection failed",err);
+    }
+}
