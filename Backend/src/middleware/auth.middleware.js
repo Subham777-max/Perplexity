a020dfbd2126b9deb461d653eabdba574856e51b
Subham777-max <subhamray865@gmail.com>
2026-03-30 11:09:19 +0530

    auth middleware is created

---
+++ Backend/src/middleware/auth.middleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+export async function authUser(req,res,next){
+    try{
+        const token = req.cookies.token;
+        if(!token){
+            const err = new Error("Unauthorized");
+            err.status = 401;
+            return next(err);
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }catch(err){
+        err.status = 401;
+        err.message = "Invalid token";
+        return next(err);
+    }
+}
