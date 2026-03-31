76075e22b9e00d65519b4a9ad0b7eab4e934cc61
Subham777-max <subhamray865@gmail.com>
2026-03-31 11:13:49 +0530

    setup cors and morgan

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
 import errorHandler from "./middleware/errorHandler.middleware.js";
 import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
