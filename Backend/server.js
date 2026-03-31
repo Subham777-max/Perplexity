b4311eb5670e1c26f3737020e09a45d27a1ac31b
Subham777-max <subhamray865@gmail.com>
2026-03-31 12:11:29 +0530

    server side socket io setup is done

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,13 @@
 import "dotenv/config";
 import app from "./src/app.js";
+import http from "http";
 import connectdb from "./src/config/db.js";
+import { initSocket } from "./src/sockets/server.socket.js";
 
+const server = http.createServer(app);
+initSocket(server);
 connectdb();
 
-app.listen(3000,()=>{
+server.listen(3000,()=>{
     console.log("Server is running at 3000");
 })
