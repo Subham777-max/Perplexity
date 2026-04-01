66d7640173840f4f1ee53f384898dd9e7aff6a97
Subham777-max <subhamray865@gmail.com>
2026-04-01 19:51:20 +0530

    improved version

--- Backend/src/controllers/chat.controller.js
+++ Backend/src/controllers/chat.controller.js
@@ -3,24 +3,31 @@ import chatModel from "../models/chat.model.js";
 import messageModel from "../models/message.model.js";
 export async function sendMessage(req, res, next) {
     try{
-        const { message } = req.body;
+        const { message , chat: chatId } = req.body;
 
-        const chatTitle = await generateChatTitle(message);
+        let chat = null , chatTitle = null;
 
-        const result = await generateResponse(message);
+        if(!chatId){
+            chatTitle = await generateChatTitle(message);
 
-        const chat = await chatModel.create({
-            user: req.user.id,
-            title: chatTitle,
-        });
-        
+            chat = await chatModel.create({
+                user: req.user.id,
+                title: chatTitle,
+            });
+        }
+
         const messageUser = await messageModel.create({
-            chat: chat._id,
+            chat: chatId || chat._id,
             role: "user",
             content: message,
         });
+
+        const messages = await messageModel.find({ chat: chatId || chat._id }).sort({ createdAt: 1 });
+
+        const result = await generateResponse(messages);
+
         const messageAi = await messageModel.create({
-            chat: chat._id,
+            chat: chatId || chat._id,
             role: "ai",
             content: result,
         });
