f764d826a0e7cab7c93378f9243ddf1772f38dd9
Subham777-max <subhamray865@gmail.com>
2026-04-02 20:08:46 +0530

    created the first function

--- Frontend/src/features/Chat/hooks/useChat.jsx
+++ Frontend/src/features/Chat/hooks/useChat.jsx
@@ -1,8 +1,40 @@
 import { initializeSocketConnection } from "../services/chat.socket";
 import { sendMessage, fetchChats, fetchChatMessages, deleteChat } from "../services/chat.service";
-export function useChat(){
+import { setChats, setCurrentChatId, setLoading, setError ,createNewChat , addNewMessages } from "../chat.slice";
+import { useDispatch } from "react-redux";
 
+
+export function useChat(){
+    const dispatch = useDispatch();
+
+    async function handleSendMessage(chatId, message) {
+        dispatch(setLoading(true));
+        try{
+            const data = await sendMessage(chatId, message);
+            const { chat , message: aimessage } = data;
+            dispatch(createNewChat({
+                chatId: chat._id,
+                title: chat.title,
+            }));
+            dispatch(addNewMessages({
+                chatId: chat._id,
+                content: message,
+                role: "user",
+            }));
+            dispatch(addNewMessages({
+                chatId: chat._id,
+                content: aimessage.content ,
+                role: aimessage.role,
+            }));
+            dispatch(setCurrentChatId(chat?._id));
+        }catch(err){
+            dispatch(setError(err.message));
+        }finally{
+            dispatch(setLoading(false));
+        }
+    }
     return{
         initializeSocketConnection,
+        handleSendMessage,
     }
 }
